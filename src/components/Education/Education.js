2f1b2e744cc92030e873cb2298d7d32ff389254a
Tara Lee <tlee379@uwo.ca>
2022-06-03 13:59:11 -0400

    education section

---
+++ src/components/Education/Education.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Education.css'
+import {Component} from 'react'
+
+class Education extends Component {
+
+  render(){
+    return (
+        <div className='section'>
+        <div className='title-container'>
+            <h5 className='title-text'>a history of my education</h5>
+        </div>
+        </div>
+  )
+  }
+
+}
+
+export default Education
